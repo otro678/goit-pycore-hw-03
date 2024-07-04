a9764de0ea909126474308a5ac2ede78d916c093
Serhii K <serhii.kozachenko.92@gmail.com>
2024-07-04 17:10:40 +0300

    Added parse error handling

--- ex01.py
+++ ex01.py
@@ -1,8 +1,15 @@
 from datetime import datetime
+
 def get_days_from_today(date : str):
-   datetime_date = datetime.strptime(date, '%Y-%m-%d')
-   delta = datetime_date - datetime.now()
-   return delta.days
+   try:
+     datetime_date = datetime.strptime(date, '%Y-%m-%d')
+     delta = datetime_date - datetime.now()
+     return delta.days
+   except ValueError:
+      return None
 
 days_diff = get_days_from_today("2024-07-01")
-print(f"Difference in days from NOW is {days_diff}")
+if days_diff is None:
+   print("Date has an incorrect format")
+else:
+   print(f"Difference in days from NOW is {days_diff}")
